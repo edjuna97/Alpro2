f7b6ac307abc3fce27a83c0764f0893102eae71f
Edi Junaedi <32472308+edjuna97@users.noreply.github.com>
2017-10-10 22:15:38 +0700

    Add files via upload

---
+++ pertemuan3/modular.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+void prosedurSD ();
+void prosedurSMP ();
+void prosedurSMK ();
+void prosedurSARJANA ();
+
+
+int main() {
+	prosedurSD();
+	prosedurSMP();
+	prosedurSMK();
+	prosedurSARJANA();
+
+return 0;
+	}
+
+void prosedurSD () 
+{
+	cout<<" 1. umur 6 tahun masuk kelas 1 SD"<<endl;
+	cout<<" 2. naik kelas 2 SD"<<endl;
+	cout<<" 3. naik kelas 3 SD"<<endl;
+	cout<<" 4. naik kelas 4 SD"<<endl;
+	cout<<" 5. naik kelas 5 SD"<<endl;
+	cout<<" 6. naik kelas 6 SD"<<endl;
+	cout<<" 7. LULUS SD"<<endl;
+ }
+
+void prosedurSMP () 
+{
+	cout<<" 8. umur 12 tahun masuk ke kelas 1 SMP"<<endl;
+	cout<<" 9. naik kelas 2 SMP"<<endl;
+	cout<<" 10. naik kelas 3 SMP"<<endl;
+ }
+
+void prosedurSMK ()
+{
+	cout<<" 11. umur 15 masuk ke kelas 1 SMK"<<endl;
+	cout<<" 12. naik kelas 2 SMK"<<endl;
+	cout<<" 13. naik kelas 3 SMK"<<endl;
+	cout<<" 14. LULUS SMK"<<endl;
+ }
+
+void prosedurSARJANA () 
+{
+	cout<<" 17. Smester 1 "<<endl;
+	cout<<" 18. Smester 2 "<<endl;
+	cout<<" 19. Smester 3 "<<endl;
+	cout<<" 20. Smester 4 "<<endl;
+	cout<<" 21. Smester 5 "<<endl;
+	cout<<" 22. Smester 6 "<<endl;
+	cout<<" 23. Smester 7 "<<endl;
+	cout<<" 24. Smester 8 "<<endl;
+	cout<<" 25. LULUS dan menjadi SARJANA dan hasilnya MEMUASKAN!!! "<<endl;
+ }
